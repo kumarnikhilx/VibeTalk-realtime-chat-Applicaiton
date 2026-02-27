f623dfb6a9d70870316953ec83d1495c767622ea
kumarnikhilx <kumarnikhilx007@gmail.com>
2026-02-27 13:46:51 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.FRONTEND_URL,
+    credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
